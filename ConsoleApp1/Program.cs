56182b728a168a86a6b64c04f13a73721ee97f32
DESKTOP-CJNH6M9\zcz-1 <Kosmos-msk@yandex.ru>
2023-09-14 18:35:06 +0300

    fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
-            Console.WriteLine("hello ttt");
+            Console.WriteLine("hello slava");
             Console.Read();
         }
     }
