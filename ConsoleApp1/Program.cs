859bba0125e43596c60d0d62350c483b6696230b
DESKTOP-CJNH6M9\zcz-1 <Kosmos-msk@yandex.ru>
2023-09-14 19:40:02 +0300

    fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,12 +8,51 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        static void PrintTable()
+        {
+            for (int i = 2; i < 10; i = i + 1)
+            {
+                for(int j = 1; j < 11; j = j + 1)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                    //Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+            
+        }
+
+        static void samsing()
+        {
+            Console.WriteLine(15.0/23.0);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
-            Console.WriteLine("some code");
-            Console.WriteLine("hello slava");
+            samsing();
+
+            Console.WriteLine();
+
+            PrintTable();
+            //double x = 15;
+            //double y = 23;
+            //Console.WriteLine(x / y);
+            //bool lol = false;
+            //if (!lol) // lol == false 
+            //{
+            //    Console.WriteLine("lol = false");
+
+            //}
+            //if (lol) // lol == true
+            //{
+            //    Console.WriteLine("lol = true");
+            //}
+            //Console.WriteLine("hello world");
+            //Console.WriteLine("some code\n\n\n\n"); // /n убирает строку
+            //Console.WriteLine("hello slava");
             Console.Read();
         }
     }
 }
+
